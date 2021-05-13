bc8c333575ad56e11e67d2f5e56d2d1d50f98fa2
Adan Constanzo <aconstanzolan@gmail.com>
2021-05-13 07:58:47 -0700

    Added new mongodb options for new verion of mongo. Also added older version of google maps url.

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var app = express();
 app.use(require('./controllers'));
 app.set('port', (process.env.PORT || 8084));
 
-mongoose.connect("mongodb://localhost/djkstra", { useNewUrlParser: true });
+var mongooseOptions = { 
+	useMongoClient: true
+};
+
+mongoose.connect("mongodb://localhost:27017/djkstra", mongooseOptions);
 
 app.listen(app.get('port'))
